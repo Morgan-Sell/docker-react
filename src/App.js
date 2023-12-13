37d63c1c842a9132dacd42eea290748f3de62ba6
Morgan-Sell <morganpsell@gmail.com>
2023-12-13 14:23:12 -0500

    revise deploy.yaml. change app.js. create .dockerignore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          EBS Docker successfully deployed!
         </p>
         <a
           className="App-link"
